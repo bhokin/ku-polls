581823e2c4cb1d37ec21aa5dcca5317297d60d8a
Bhokin <bhokin.w@ku.th>
2021-09-11 21:01:48 +0700

    add a condition for vote

--- polls/views.py
+++ polls/views.py
@@ -48,9 +48,12 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        if question.can_vote():
+            selected_choice.votes += 1
+            selected_choice.save()
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        else:
+            return HttpResponseRedirect(reverse('polls:index'))
